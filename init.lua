61d9d843d4d510c9804276024103c67f5eee11ea
Jesterknightofficial <jesterknight@tutamail.com>
2025-08-24 17:00:24 +0000

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,29 @@
+vim.cmd.colorscheme("zaibatsu")
+
+
+
+-- Text Layout
+vim.o.number = true
+vim.o.relativenumber = true 
+vim.o.cursorline = true
+vim.o.wrap = false
+vim.o.scrolloff = 10 
+vim.o.sidescrolloff = 8 
+vim.o.smartindent = true 
+
+-- Search Settings
+vim.o.ignorecase = true
+vim.o.smartcase = true 
+vim.o.hlsearch = true 
+vim.o.incsearch = true 
+
+-- Visual Settings
+vim.o.termguicolors = true
+
+-- File Handling
+vim.o.swapfile = false
+vim.o.writebackup = false
+vim.o.autoread = true
+
+
+
